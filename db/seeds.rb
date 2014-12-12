f53eaca9c53cc20d69a5f7491c859bd0b0a859b7
Mike Horn <mhorn918@gmail.com>
2014-12-12 16:48:21 -0500

    added faker data file

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+require 'faker'
+
+30.times do
+  User.create(
+    name: Faker::Name.name,
+    password: Faker::Internet.password,
+  )
+end
+
+30.times do
+  Coin.create(
+    content: Faker::Company.bs,
+    user_id: 1 + rand(User.all.length)
+  )
+end
+
+
+
