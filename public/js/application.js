2631652db2dc3cb0bde454ed2bb5ab7697b89ebd
Don J. Kahn <kahdojay@gmail.com>
2014-12-13 18:20:19 -0500

    fixed fadeIn fadeOut function on index

--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,8 @@
 $(document).ready(function() {
-
   $(".graphic-index").click(function() {
-    // fade out, toggle background, then fade back in
-    $(".graphic-index").fadeOut("slow");
-    $(".graphic-index").css("visibility", "hidden");
-    $(".graphic-index").delay(500).css("background-image", "url('../img/coin-hi.png')");
-    $(".graphic-index").delay(500).css("visibility", "visible");
-    $(".graphic-index").fadeIn("slow");
-  })
-    // $(".graphic-index").fadeToggle("slow", function () {
-    //   $(this).css
-    //   "../img/coin-hi.png")
-    // $(".coin-graphic").fadeToggle().delay(500).fadeToggle("slow", function() {
-      // $(".coin-graphic").css("display", "table-cell");
-    // });
-  // });
-
-  // $(".coin-graphic").click(function() {
-  //   $(".coin-graphic").reload();
-  // });
+    $(".graphic-index").fadeOut(function(){
+      $(this).css("background-image", "url('../img/coin-hi.png')");
+      $(".graphic-index").fadeIn("slow");
+    });
+  });
 });
