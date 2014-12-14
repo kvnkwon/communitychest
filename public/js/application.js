7af63f2d41ac1a7f6cf91b3ca0f54d6a87962923
Kevin <kvnkwon@gmail.com>
2014-12-14 16:25:17 -0500

    implemented mousetrap and added font

--- public/js/application.js
+++ public/js/application.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  $(".graphic-index").click(function() {
-     $.ajax({
-        url: "/random_coin",
-        type: "GET",
-        dataType: "json"
-      }).done(function(response){
-        console.log(response);
-          $(".graphic-index").fadeOut(function(){
-          $(this).css("background-image", "url('../img/coin-hi.png')");
-          $("#coin-advice").css("visibility", "visible");
-          $("#coin-advice").html(response.content);
-          $(".graphic-index").fadeIn("slow");
-        });
+  Mousetrap.bind('n', function() {
+    $.ajax({
+      url: "/random_coin",
+      type: "GET",
+      dataType: "json"
+    }).done(function(response){
+      console.log(response);
+        $(".graphic-index").fadeOut(function(){
+        $(this).css("background-image", "url('../img/coin-hi.png')");
+        $("#coin-advice").css("visibility", "visible");
+        $("#coin-advice").html('"' + response.content + '"');
+        $(".graphic-index").fadeIn("slow");
       });
-  });
+    });
+  }, 'keyup');
 });
