afffac9334219eb915fc9b7ec56a76187d79bad3
Don J. Kahn <kahdojay@gmail.com>
2014-12-13 19:14:41 -0500

    Implement ajax call on click.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,9 @@
 get '/' do
   # Look in app/views/index.erb
-  @test = 'TEST'
   @random_coin = Coin.all.sample
   erb :index
+end
+
+get '/random_coin' do
+  Coin.all.sample.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
   $(".graphic-index").click(function() {
-    $(".graphic-index").fadeOut(function(){
-      $(this).css("background-image", "url('../img/coin-hi.png')");
-      $(".graphic-index").fadeIn("slow");
-    });
+     $.ajax({
+        url: "/random_coin",
+        type: "GET",
+        dataType: "json"
+      }).done(function(response){
+        console.log(response);
+          $(".graphic-index").fadeOut(function(){
+          $(this).css("background-image", "url('../img/coin-hi.png')");
+          $("#coin-advice").css("visibility", "visible");
+          $("#coin-advice").html(response.content);
+          $(".graphic-index").fadeIn("slow");
+        });
+      });
   });
 });
